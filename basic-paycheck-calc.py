accf0c4fb666d42bb54163fc58d42b87c8cd017d
thejardilak19 <93497128+thejardilak19@users.noreply.github.com>
2022-07-07 04:41:08 -0800

    Update basic-paycheck-calc.py
    Rewrote the program from scratch

--- basic-paycheck-calc.py
+++ basic-paycheck-calc.py
@@ -1,35 +1,52 @@
-#project done just for fun
+# taking input for the program to work with
 
-print('Welcome. What would you like to do?')
-info = input('1 : Calculate wages\n2 : Perform a simple tax bracket analysis'
-             '\n')
+hourly_wage = input('How much are you paid an hour?\n~')
+ot_wage = input('How much are you paid an hour during overtime?\n~')
+reg_hours = input('How many regular hours did you work so far?\n~')
+ot_hours = input('How many overtime hours did you work so far?\n~')
+print('Do you know your tax rate? If yes, type in decimal format.')
+tax_rate = input('If not, look at a recent paycheck. Divide your takehome pay by gross pay.\n~')
 
-hourly_wage = input('How much are you paid an hour?\n:')
-overtime_wage = input('How much are you paid an hour on overtime?\n:')
+# converting the numbers to something the program can work with
 hourly_wage = float(hourly_wage)
-overtime_wage = float(overtime_wage)
-
-
-reg_hours = input('How many regular hours worked?\n:')
-ot_hours = input('How many overtime hours?\n:')
-
+ot_wage = float(ot_wage)
 reg_hours = float(reg_hours)
 ot_hours = float(ot_hours)
-total = 0
-reg_pay = (reg_hours * hourly_wage)
-ot_pay = (ot_hours * overtime_wage)
+tax_rate = float(tax_rate)
 
-total = total + reg_pay + ot_pay
-after_tax = total * 0.8549
-taken = total - after_tax
+# doing the math
 
-after_tax = round(after_tax, 2)
-taken = round(taken, 2)
+reg_pay = hourly_wage * reg_hours
+ot_pay = ot_wage * ot_hours
 
-print('After tax, you\'re getting:')
-print('$' + str(after_tax))
-print('You were shafted for:')
-print('$' + str(taken))
+gross = reg_pay + ot_pay
+net = gross * tax_rate
 
-tax_analysis = input('Would you like to perform a tax analysis, on how much you\'re projected to make in a year, including what tax bracket you\'re in? \n(y/n):')
-elif tax_analysis == 'n'
+# declaring tax brackets for 2022
+# 10% = $0 to $10,275
+# 12% = @10,275 to $41,775
+# 22% = $41,775 to $89,075
+# 24% = $89,075 to $170,050
+# 32% = $170,050 to $215,950
+# 35% = $215,950 to $539,500
+# 37% = $539,500 and higher
+
+print('Your hours should get you:\n${0:.2f}'.format(gross))
+print('This is how much you\'ll actually get:\n${0:.2f}'.format(net))
+taken = gross - net
+print('This is how much was taken out:\n${0:.2f}'.format(taken))
+
+yearly_projected = net * 26
+
+if 0 < yearly_projected < 10275:
+    print('You should be in the 10% tax bracket')
+elif 10275 < yearly_projected < 41775:
+    print('You should be in the 12% tax bracket')
+elif 41775 < yearly_projected < 89075:
+    print('You should be in the 22% tax bracket')
+elif 89075 < yearly_projected < 170050:
+    print('You should be in the 24% tax bracket')
+elif 170050 < yearly_projected < 215950:
+    print('You should be in the 32% tax bracket')
+elif 215950 < yearly_projected:
+    print('You make too much to really care anyway')
